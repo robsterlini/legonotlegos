9ecb2cded114d1f5453d6449df206012ec9b0ac5
Rob Sterlini <github@robsterlini.co.uk>
2020-03-09 13:34:49 +0000

    feat(postcss): Enable autoprefixing, importing, vars, minfication and inlining
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,26 @@
+const postcss = require('postcss');
+
 module.exports = function(config) {
+
+  config.addNunjucksAsyncFilter('postcss', function(code, callback) {
+    const css = postcss([
+      require('postcss-import')({
+        path: [
+          'src/site/_includes/css/',
+        ],
+      }),
+      require('postcss-simple-vars')({
+        silent: true
+      }),
+      require('autoprefixer')(),
+      require('cssnano')(),
+    ])
+    .use(require("postcss-import")())
+    .process(code)
+    .then(function(result) {
+      callback(null, result.css);
+    });
+  });
 
   config.addLayoutAlias('default', 'base.njk');
 
